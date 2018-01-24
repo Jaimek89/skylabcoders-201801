3ae35c55ac9052092d0858c2e2a79c8c06d08ad4
Jaimek89 <jaimek89@gmail.com>
2018-01-24 18:47:59 +0100

    24/01

---
+++ course/topics/javascript/challenges/scripts/main.js
@@ -0,0 +1,7 @@
+function text(word){
+    
+};
+text('Jaime');
+
+// text('something').wrap('$').wrap('[',']').wrap('{','}').wrap('<','>').wrap('#')
+// output => #<{[$something$]}>#
--- course/topics/javascript/high order functions/scripts/main.js
+++ course/topics/javascript/high order functions/scripts/main.js
@@ -2,44 +2,46 @@
 
 //Write a high order function that take the array of months and create a new array that contains the same months but with the first letter in upper case.
 
-var months = ['january','february','march','april','may','june','july','august', 'september','october','november','december']
-function upperMonth(){
-    var upperLetter = months.map(function(x){
-        return x.slice(0,1).toLocaleUpperCase()
+var months = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december']
+function upperMonth() {
+    var upperLetter = months.map(function (x) {
+        return x.slice(0, 1).toLocaleUpperCase() + x.slice(1, x.length)
     })
+    console.log(upperLetter)
 }
 upperMonth()
-
-var month = 'january'
-var letter = month.slice(0,1)
-console.log(letter)
-var result = letter.toLocaleUpperCase()
-console.log(result)
-
 
 //## filter
 
 //Write a high order function that take the array of months and create a new array that contains only the months that begin with j.
 
-var months = ['january','february','march','april','may','june','july','august', 'september','october','november','december']
+var months = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december']
+function jMonths() {
+    function filterMonth(month) {
+        return month.slice(0, 1) === 'j';
+    }
+    var months = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december'].filter(filterMonth)
+    console.log(months)
+}
+jMonths()
 
 //## reduce
 
 //Write a high order function that counts all the vowels of the following text.
 
 var countVowels;
-(function(){
+(function () {
     var regex = /[aáeéiíoóuú\+]+/g
 
-    countVowels = function(text){
-        return text.split('').reduce(function(vowelsCount, letter){
+    countVowels = function (text) {
+        return text.split('').reduce(function (vowelsCount, letter) {
 
-            if (letter.toLowerCase().match(regex)){
+            if (letter.toLowerCase().match(regex)) {
                 return vowelsCount += 1
             }
 
             return vowelsCount
-        },0)
+        }, 0)
     }
 })();
 
@@ -52,16 +54,22 @@ var loremIpsum = 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed 
 
 //Write a high order function that returns true if all months have five letters or more, and if some month has five letters or more.
 
-var months = ['january','february','march','april','may','june','july','august', 'september','october','november','december']
+function esMonth() {
+    var months = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december'];
+
+    function everyMonth(month) {
+        return month.length > 5;
+    };
+    var eMonth = months.every(everyMonth);
+    console.log('This function should return false, because not every months have at least 5 letter, so... ' + eMonth);
+
+    var sMonth = months.some(everyMonth);
+    console.log('This function should return true, because some month has at least 5 letter, so... ' + sMonth);
+};
+esMonth();
 
 //## Callback
 
 //Write a callback function that receives an age and a callback function, if age is equal or great than 18, call the callback function to allow enter in a bar, if hasn't 18 the entrance is not allowed.
 
-function accesBar(age, callback){
-    age>=18 ? callback(true) : callback(false);
-}
-function advisor(allowed){
-    console.log(allowed? 'the entrance is allowed' : 'the entrance is not allowed');
-}
-accesBar(8,advisor);
+
